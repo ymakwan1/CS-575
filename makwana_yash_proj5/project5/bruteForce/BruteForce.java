1202a098845bfb5b18be0d338c1963fa2059b371
ymakwan1 <ymakwan1@binghamton.edu>
2023-04-21 01:08:57 -0400

    TODO stub added

--- makwana_yash_proj5/project5/bruteForce/BruteForce.java
+++ makwana_yash_proj5/project5/bruteForce/BruteForce.java
@@ -1,5 +1,12 @@
 package project5.bruteForce;
 
+import project5.utils.ExceptionHandler;
+
 public class BruteForce {
-    
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            ExceptionHandler.handleException(null, "Usage : java project5/bruteForce/BruteForce knapsack01.txt");
+        }
+        //TODO - Check if file exists, if not raise exception and ask to run createFile
+    }
 }
